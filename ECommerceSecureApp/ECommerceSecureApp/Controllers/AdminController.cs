3d72ad12cf589cf1473f859f57e2538e43cb31df
Ritu Arora <jsr.ritu.jsr@gmail.com>
2025-09-04 13:54:27 -0400

    Admin and Customer roles added

---
+++ ECommerceSecureApp/ECommerceSecureApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceSecureApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View(); // Create Views/Admin/Dashboard.cshtml
+        }
+
+        public IActionResult ManageProducts()
+        {
+            // Return a view with product management tools
+            return View();
+        }
+
+        // Add actions like CreateProduct, EditProduct, DeleteProduct
+    }
+}
---
+++ ECommerceSecureApp/ECommerceSecureApp/SeedData/RoleSeeder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ECommerceSecureApp.SeedData
+{
+    public class RoleSeeder
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string[] roles = { "Admin", "Customer" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            // Create admin user
+            var adminEmail = "admin@store.com";
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true };
+                await userManager.CreateAsync(adminUser, "Admin@123"); // Use a strong password
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+
+    }
+}
