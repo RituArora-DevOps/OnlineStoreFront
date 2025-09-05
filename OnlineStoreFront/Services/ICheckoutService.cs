9c17c385451b7267357cb25d5a26476fed04b4d5
FYFYBai <93832388+FYFYBai@users.noreply.github.com>
2025-09-04 22:30:11 -0400

    removed dupicates

--- OnlineStoreFront/Services/ICheckoutService.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnlineStoreFront.Models.ViewModels;
-
-namespace OnlineStoreFront.Services;
-
-public interface ICheckoutService
-{
-    Task<CartVM> GetSummaryAsync(string userId);
-    Task<long> PlaceOrderAsync(string userId, CheckoutAddressVM addr);
-}
